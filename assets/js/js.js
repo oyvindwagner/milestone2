af322642b482b09fc561f345d8c158db86c99994
Øyvind Wagner <oyvind.wagner@gmail.com>
2020-06-03 08:54:41 +0000

    Created map box base map with API. Added dependencies.

---
+++ assets/js/js.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoib3l3YSIsImEiOiJja2F6M2gyc3owOXljMzNwajNxNGd1MGhvIn0.2I30kWkJcq51VsV0Qmq0Nw';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: [-77.034084, 38.909671],
+  zoom: 14
+});
