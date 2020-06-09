39167994e136f042eaf2c4295ce9abe2b0f0d335
Øyvind Wagner <oyvind.wagner@gmail.com>
2020-06-09 05:57:28 +0000

    Added page loader. Updated code comments.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
+// basemap
 const map = L.map('map', {
       center: [15.326572, -76.157227],
       zoom: 3
     });
     L.esri.basemapLayer('Oceans').addTo(map);
     L.esri.basemapLayer('OceansLabels').addTo(map);
+// basemap
+
+// loader
+var myVar;
+
+function loaderFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("pageLoader").style.display = "block";
+}
+// loader
